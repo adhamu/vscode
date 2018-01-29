c1584f3bc9d095c8ed99b0c2307479d19d6818a8
Amit Dhamu <DHAMUA@cpwplc.com>
2018-01-29 11:23:56 +0000

    Update JSON theme

---
+++ extensions/herrmannplatz.npm-dependency-links-1.0.0/extension.js
@@ -0,0 +1,48 @@
+const { languages, Uri, DocumentLink, Range } = require('vscode');
+const path = require('path');
+
+function buildLink(line, lineIndex, package) {
+    const startCharacter = line.text.indexOf(package);
+    const endCaracter = startCharacter + package.length;
+    const linkRange = new Range(lineIndex, startCharacter, lineIndex, endCaracter);
+    const linkUri = Uri.parse(`https://www.npmjs.com/package/${package}`);
+    return new DocumentLink(linkRange, linkUri);
+}
+
+exports.activate = function (context) {
+    const disposable = languages.registerDocumentLinkProvider(['javascript', { pattern: '**/package.json' }], {
+        provideDocumentLinks(document, token) {
+            let links = [];
+            let lineIndex = 0;
+            let shouldCheckForDependency = false;
+            
+            while (lineIndex < document.lineCount) {
+                const line = document.lineAt(lineIndex);
+
+                if (shouldCheckForDependency) {
+                    // no need to check for dependencies if block ended
+                    if (line.text.includes('}')) {
+                        shouldCheckForDependency = false;
+                    } else {
+                        // find dependecy
+                        const matches = line.text.match(/"(.*?)"/);
+                        
+                        if (matches) {
+                            links.push(buildLink(line, lineIndex, matches[1]));
+                        }
+                    }
+
+                } else {
+                    // check if we are in a dependencies block
+                    shouldCheckForDependency = /"(.*?)dependencies"/i.test(line.text);
+                }
+                    
+                lineIndex += 1;
+            }
+
+            return links;
+        }
+    });
+
+    context.subscriptions.push(disposable)
+};
