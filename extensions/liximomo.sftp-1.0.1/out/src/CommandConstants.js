f2ad731f8da9ce2d89ba8501a33435733939f931
Amit Dhamu <DHAMUA@cpwplc.com>
2018-02-01 08:27:30 +0000

    Remove intellinsense and add intelephense

--- extensions/liximomo.sftp-1.0.1/out/src/CommandConstants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIG = 'sftp.config.default';
-exports.SYNC_TO_REMOTE = 'sftp.sync.remote';
-exports.SYNC_TO_LOCAL = 'sftp.sync.local';
-exports.UPLOAD = 'sftp.trans.remote';
-exports.DOWNLOAD = 'sftp.trans.local';
-//# sourceMappingURL=CommandConstants.js.map
--- extensions/liximomo.sftp-1.0.1/out/src/modules/localFs.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const LocalFileSystem_1 = require("../model/Fs/LocalFileSystem");
-const fs = new LocalFileSystem_1.default(path);
-exports.default = fs;
-//# sourceMappingURL=localFs.js.map
