a7057c6a04beca1691a815eea5b9cb9e884e5b87
Amit Dhamu <DHAMUA@cpwplc.com>
2018-02-05 17:12:20 +0000

    Update

--- extensions/liximomo.sftp-1.0.2/out/src/helper/promsiseAll.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=promsiseAll.js.map
--- extensions/liximomo.sftp-1.0.2/out/src/modules/exception.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function SlientException(message) {
-    this.message = message;
-    this.name = 'SlientException';
-}
-exports.SlientException = SlientException;
-//# sourceMappingURL=exception.js.map
